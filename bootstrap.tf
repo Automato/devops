5a3ec8126b5da628fe8031c72f7d1c6c2dd4bcc2
David J Felix <felix.davidj@gmail.com>
2016-06-23 11:41:56 -0400

    Add comments to bootstrap

--- bootstrap.tf
+++ bootstrap.tf
@@ -1,3 +1,5 @@
+# Automato Administrative Log Bucket
+# This global s3 bucket is designed to hold logs for the Automato admin bucket actions
 resource "aws_s3_bucket" "automato_admin_log_bucket" {
   bucket = "automato-admin-log"
   acl = "log-delivery-write"
@@ -8,6 +10,11 @@ resource "aws_s3_bucket" "automato_admin_log_bucket" {
   }
 }
 
+
+# Automato Administrative Bucket
+# This global s3 bucket is designed to hold administrative shared files for the Automato organization
+# This is part of bootstrap because the bucket will be used to hold environment tfstate files,
+# results of packer builds, and other shared resources used in provisioning
 resource "aws_s3_bucket" "automato_admin_bucket" {
   bucket = "automato-admin"
   acl = "private"
