9b0a117f6d77c6a2f6d07fae8782d6d18429e734
David J Felix <felix.davidj@gmail.com>
2016-06-27 13:09:16 -0400

    Make sure to get modules

--- terraform/env-aws-us-east-1-development/init.sh
+++ terraform/env-aws-us-east-1-development/init.sh
@@ -4,3 +4,5 @@ terraform remote config -backend=s3 \
                         -backend-config="bucket=automato-admin" \
                         -backend-config="key=terraform/aws-us-east-1-development.tfstate" \
                         -backend-config="region=us-east-1"
+
+terraform get
