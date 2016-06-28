9b6d320de138601c59486b396897f604356719db
David J Felix <felix.davidj@gmail.com>
2016-06-27 20:05:08 -0400

    Lay groundwork for east subnets

--- terraform/env-aws-us-east-1-development/main.tf
+++ terraform/env-aws-us-east-1-development/main.tf
@@ -1,7 +1,19 @@
-module "aws-vpc" {
+module "vpc" {
   source = "../modules/aws-vpc"
   name = "Automato Development"
   cidr = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
+}
+
+module "subnet-a" {
+}
+
+module "subnet-b" {
+}
+
+module "subnet-d" {
+}
+
+module "subnet-e" {
 }
