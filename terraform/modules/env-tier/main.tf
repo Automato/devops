bd8f0954a20bffe78cea05b546d8e0e4c6cfb39a
David J Felix <felix.davidj@gmail.com>
2017-01-15 17:05:26 -0500

    Create multiple default subnets

--- terraform/modules/env-tier/main.tf
+++ terraform/modules/env-tier/main.tf
@@ -8,13 +8,63 @@ resource "aws_vpc" "tier_vpc" {
   }
 }
 
-resource "aws_subnet" "tier_subnet" {
+resource "aws_subnet" "web_subnet" {
   count = "${length(var.vpc_zones)}"
   vpc_id = "${aws_vpc.tier_vpc.id}"
   availability_zone = "${element(var.vpc_zones, count.index)}"
   cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, count.index)}"
   tags {
-    Name = "${format("%s %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+    Name = "${format("%s web %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+  }
+}
+
+resource "aws_subnet" "web_db_subnet" {
+  count = "${length(var.vpc_zones)}"
+  vpc_id = "${aws_vpc.tier_vpc.id}"
+  availability_zone = "${element(var.vpc_zones, count.index)}"
+  cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, length(var.vpc_zones) + count.index)}"
+  tags {
+    Name = "${format("%s web db %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+  }
+}
+
+resource "aws_subnet" "api_subnet" {
+  count = "${length(var.vpc_zones)}"
+  vpc_id = "${aws_vpc.tier_vpc.id}"
+  availability_zone = "${element(var.vpc_zones, count.index)}"
+  cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, (length(var.vpc_zones) * 2) + count.index)}"
+  tags {
+    Name = "${format("%s api %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+  }
+}
+
+resource "aws_subnet" "api_db_subnet" {
+  count = "${length(var.vpc_zones)}"
+  vpc_id = "${aws_vpc.tier_vpc.id}"
+  availability_zone = "${element(var.vpc_zones, count.index)}"
+  cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, (length(var.vpc_zones) * 3) + count.index)}"
+  tags {
+    Name = "${format("%s api db %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+  }
+}
+
+resource "aws_subnet" "internal_subnet" {
+  count = "${length(var.vpc_zones)}"
+  vpc_id = "${aws_vpc.tier_vpc.id}"
+  availability_zone = "${element(var.vpc_zones, count.index)}"
+  cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, (length(var.vpc_zones) * 4) + count.index)}"
+  tags {
+    Name = "${format("%s internal %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+  }
+}
+
+resource "aws_subnet" "internal_db_subnet" {
+  count = "${length(var.vpc_zones)}"
+  vpc_id = "${aws_vpc.tier_vpc.id}"
+  availability_zone = "${element(var.vpc_zones, count.index)}"
+  cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, (length(var.vpc_zones) * 5) + count.index)}"
+  tags {
+    Name = "${format("%s internal db %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
   }
 }
 
