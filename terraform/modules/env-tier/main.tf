28a1df935c43188eee41e63ff0425a70fc4abcea
David J. Felix <DavidJFelix@users.noreply.github.com>
2017-01-16 18:24:37 -0500

    Add comments for external subnet and vpc

--- terraform/modules/env-tier/main.tf
+++ terraform/modules/env-tier/main.tf
@@ -1,3 +1,4 @@
+# The VPC created for this tier.
 resource "aws_vpc" "tier" {
   cidr_block = "${var.vpc_cidr_block}"
   instance_tenancy = "${var.vpc_instance_tenancy}"
@@ -9,6 +10,12 @@ resource "aws_vpc" "tier" {
 }
 
 
+### EXTERNAL SUBNET
+# The base external subnets.
+# One subnet is allocated for each availability zone specified for the module.
+# External subnets are designed for services or systems designed to talk to external customers on the internet.
+# By default only one CIDR/24 block is allocated for it in each AZ, but additional subnets can be created and
+# associated with the route table and network ACLs to expand it.
 resource "aws_subnet" "external" {
   count = "${length(var.vpc_zones)}"
   vpc_id = "${aws_vpc.tier.id}"
@@ -19,6 +26,7 @@ resource "aws_subnet" "external" {
   }
 }
 
+# The external route table is shared for all external subnets
 resource "aws_route_table" "external" {
   vpc_id = "${aws_vpc.tier.id}"
   tags {
@@ -26,23 +34,28 @@ resource "aws_route_table" "external" {
   }
 }
 
+# The route table association is created for each AZ subnet, additional subnets will need to provide this themselves
 resource "aws_route_table_association" "external" {
   count = "${length(var.vpc_zones)}"
   route_table_id = "${aws_route_table.external.id}"
   subnet_id = "${element(aws_subnet.external.*.id, count.index)}"
 }
 
+# The external route table has a route entry to allow access to the public internet via internet gateway
 resource "aws_route" "external_to_internet" {
   route_table_id = "${aws_route_table.external.id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = "${aws_internet_gateway.tier.id}"
 }
 
+# An elastic IP is allocated for each availability zone
 resource "aws_eip" "external" {
   count = "${length(var.vpc_zones)}"
   vpc = true
 }
 
+# A NAT gateway is allocated within each external AZ subnet to provide internal systems limited external access
+# Expanding the number of external subnets should not require more NAT gateways, as one is allocated in each AZ
 resource "aws_nat_gateway" "external" {
   count = "${length(var.vpc_zones)}"
   allocation_id = "${element(aws_eip.external.*.id, count.index)}"
@@ -53,6 +66,7 @@ resource "aws_nat_gateway" "external" {
 }
 
 
+### EXTERNAL DB SUBNET
 resource "aws_subnet" "external_db" {
   count = "${length(var.vpc_zones)}"
   vpc_id = "${aws_vpc.tier.id}"
