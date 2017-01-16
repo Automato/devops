5aa1afca51d8d5f17497bf2ae86c67c95fbbeb7d
David J Felix <felix.davidj@gmail.com>
2017-01-15 22:28:34 -0500

    Reduce tier to external/internal and add routes
    - External is a group for external facing systems with data which
      may be accessible via services.
    - Internal is a group for internal (or vpn) facing systems with that
      which may be accessible via services
    - Each "private" subnet will utilize a nat located in the same AZ with
      in the public subnet for external traffic, as allowed by network acl.
      This will allow self-hosted dbs to reach external updates. and
      modules.

--- terraform/modules/env-tier/main.tf
+++ terraform/modules/env-tier/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "tier_vpc" {
+resource "aws_vpc" "tier" {
   cidr_block = "${var.vpc_cidr_block}"
   instance_tenancy = "${var.vpc_instance_tenancy}"
   enable_dns_support = "${var.vpc_enable_dns_support}"
@@ -8,68 +8,148 @@ resource "aws_vpc" "tier_vpc" {
   }
 }
 
-resource "aws_subnet" "web_subnet" {
+
+resource "aws_subnet" "external" {
   count = "${length(var.vpc_zones)}"
-  vpc_id = "${aws_vpc.tier_vpc.id}"
+  vpc_id = "${aws_vpc.tier.id}"
   availability_zone = "${element(var.vpc_zones, count.index)}"
   cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, count.index)}"
   tags {
-    Name = "${format("%s web %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+    Name = "${format("%s external %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
   }
 }
 
-resource "aws_subnet" "web_db_subnet" {
+resource "aws_route_table" "external" {
+  vpc_id = "${aws_vpc.tier.id}"
+  tags {
+    Name = "${format("%s external %s", var.org_name, var.tier_name)}"
+  }
+}
+
+resource "aws_route_table_association" "external" {
   count = "${length(var.vpc_zones)}"
-  vpc_id = "${aws_vpc.tier_vpc.id}"
+  route_table_id = "${aws_route_table.external.id}"
+  subnet_id = "${element(aws_subnet.external.*.id, count.index)}"
+}
+
+resource "aws_route" "external_to_internet" {
+  route_table_id = "${aws_route_table.external.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.tier.id}"
+}
+
+resource "aws_eip" "external" {
+  count = "${length(var.vpc_zones)}"
+  vpc = true
+}
+
+resource "aws_nat_gateway" "external" {
+  count = "${length(var.vpc_zones)}"
+  allocation_id = "${element(aws_eip.external.*.id, count.index)}"
+  subnet_id = "${element(aws_subnet.external.*.id, count.index)}"
+  depends_on = [
+    "aws_internet_gateway.tier"
+  ]
+}
+
+
+resource "aws_subnet" "external_db" {
+  count = "${length(var.vpc_zones)}"
+  vpc_id = "${aws_vpc.tier.id}"
   availability_zone = "${element(var.vpc_zones, count.index)}"
   cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, length(var.vpc_zones) + count.index)}"
   tags {
-    Name = "${format("%s web db %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+    Name = "${format("%s external_db %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
   }
 }
 
-resource "aws_subnet" "api_subnet" {
+resource "aws_route_table" "external_db" {
   count = "${length(var.vpc_zones)}"
-  vpc_id = "${aws_vpc.tier_vpc.id}"
+  vpc_id = "${aws_vpc.tier.id}"
+  tags {
+    Name = "${format("%s external_db %s %s", var.org_name, var.tier_name, count.index)}"
+  }
+}
+
+resource "aws_route_table_association" "external_db" {
+  count = "${length(var.vpc_zones)}"
+  route_table_id = "${element(aws_route_table.external_db.*.id, count.index)}"
+  subnet_id = "${element(aws_subnet.external_db.*.id, count.index)}"
+}
+
+resource "aws_route" "external_db_to_nat" {
+  count = "${length(var.vpc_zones)}"
+  route_table_id = "${element(aws_route_table.external_db.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${element(aws_nat_gateway.external.*.id, count.index)}"
+}
+
+
+resource "aws_subnet" "internal" {
+  count = "${length(var.vpc_zones)}"
+  vpc_id = "${aws_vpc.tier.id}"
   availability_zone = "${element(var.vpc_zones, count.index)}"
   cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, (length(var.vpc_zones) * 2) + count.index)}"
-  tags {
-    Name = "${format("%s api %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
-  }
-}
-
-resource "aws_subnet" "api_db_subnet" {
-  count = "${length(var.vpc_zones)}"
-  vpc_id = "${aws_vpc.tier_vpc.id}"
-  availability_zone = "${element(var.vpc_zones, count.index)}"
-  cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, (length(var.vpc_zones) * 3) + count.index)}"
-  tags {
-    Name = "${format("%s api db %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
-  }
-}
-
-resource "aws_subnet" "internal_subnet" {
-  count = "${length(var.vpc_zones)}"
-  vpc_id = "${aws_vpc.tier_vpc.id}"
-  availability_zone = "${element(var.vpc_zones, count.index)}"
-  cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, (length(var.vpc_zones) * 4) + count.index)}"
   tags {
     Name = "${format("%s internal %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
   }
 }
 
-resource "aws_subnet" "internal_db_subnet" {
+resource "aws_route_table" "internal" {
   count = "${length(var.vpc_zones)}"
-  vpc_id = "${aws_vpc.tier_vpc.id}"
-  availability_zone = "${element(var.vpc_zones, count.index)}"
-  cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, (length(var.vpc_zones) * 5) + count.index)}"
+  vpc_id = "${aws_vpc.tier.id}"
   tags {
-    Name = "${format("%s internal db %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+    Name = "${format("%s internal %s %s", var.org_name, var.tier_name, count.index)}"
   }
 }
 
-resource "aws_internet_gateway" "tier_gateway" {
-  vpc_id = "${aws_vpc.tier_vpc.id}"
+resource "aws_route_table_association" "internal" {
+  count = "${length(var.vpc_zones)}"
+  route_table_id = "${element(aws_route_table.internal.*.id, count.index)}"
+  subnet_id = "${element(aws_subnet.internal.*.id, count.index)}"
+}
+
+resource "aws_route" "internal_to_nat" {
+  count = "${length(var.vpc_zones)}"
+  route_table_id = "${element(aws_route_table.internal.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${element(aws_nat_gateway.external.*.id, count.index)}"
+}
+
+resource "aws_subnet" "internal_db" {
+  count = "${length(var.vpc_zones)}"
+  vpc_id = "${aws_vpc.tier.id}"
+  availability_zone = "${element(var.vpc_zones, count.index)}"
+  cidr_block = "${cidrsubnet(var.vpc_cidr_block, 8, (length(var.vpc_zones) * 3) + count.index)}"
+  tags {
+    Name = "${format("%s internal_db %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+  }
+}
+
+resource "aws_route_table" "internal_db" {
+  count = "${length(var.vpc_zones)}"
+  vpc_id = "${aws_vpc.tier.id}"
+  tags {
+    Name = "${format("%s internal_db %s %s", var.org_name, var.tier_name, element(var.vpc_zones, count.index))}"
+  }
+}
+
+resource "aws_route_table_association" "internal_db" {
+  count = "${length(var.vpc_zones)}"
+  route_table_id = "${element(aws_route_table.internal_db.*.id, count.index)}"
+  subnet_id = "${element(aws_subnet.internal_db.*.id, count.index)}"
+}
+
+resource "aws_route" "internal_db_to_nat" {
+  count = "${length(var.vpc_zones)}"
+  route_table_id = "${element(aws_route_table.internal_db.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${element(aws_nat_gateway.external.*.id, count.index)}"
+}
+
+
+resource "aws_internet_gateway" "tier" {
+  vpc_id = "${aws_vpc.tier.id}"
   tags {
     Name = "${format("%s %s", var.org_name, var.tier_name)}"
   }
